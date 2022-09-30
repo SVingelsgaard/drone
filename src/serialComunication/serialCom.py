f6ab6e09fd3a2f4bb17a17442e2ae61047506cb0
SVingelsgaard <sigurd@vingelsgaard.no>
2022-10-01 00:22:33 +0200

    sds

--- src/serialComunication/serialCom.py
+++ src/serialComunication/serialCom.py
@@ -6,13 +6,13 @@ import time
 class SerialCom:
     def __init__(self):
 
-        self.COMPort = ""
+        self.COMPort = "COM10"#deafault com port
 
-        #self.arduino = serial(port="COM4", baudrate=9600, timeout=.1)
+
+        self.arduino = serial.Serial(port=self.COMPort, baudrate=9600, timeout=.1)
         time.sleep(3)#literry dying without this idk.
 
     def cycle(self):
         self.data = str()#+100 to make sure data uses 3 digits.
         #self.arduino.write(bytes(self.data, 'utf-8'))
-        print("ree")
 

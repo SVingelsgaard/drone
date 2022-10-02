01e435c4d72dc0ceca51af7ec2d4a2d4ed012c56
SVingelsgaard <sigurd@vingelsgaard.no>
2022-10-02 20:59:27 +0200

    rs

--- src/arduino/transmitter/radioTransmitter_uno.ino
+++ src/arduino/transmitter/radioTransmitter_uno.ino
@@ -8,18 +8,19 @@
 
 const byte slaveAddress[5] = {'R','x','A','A','A'};
 
+const unsigned int MAX_MESSAGE_LENGTH = 12;
+
 RF24 radio(CE_PIN, CSN_PIN); // Create a Radio
 
 String dataToSendString = "000000000000";
 //char dataToSend[12] = "000000000000";
 int dataToSend = 0;
 
-byte LFSpeed;
-byte RFSpeed;
-byte LBSpeed;
-byte RBSpeed;
+String LFSpeed;
+String RFSpeed;
+String LBSpeed;
+String RBSpeed;
 
-int pot;
 
 unsigned long currentMillis;
 unsigned long prevMillis;
@@ -33,15 +34,19 @@ void setup() {
     Serial.println("SimpleTx Starting");
 
     radio.begin();
+    Serial.println("1");
     radio.setDataRate( RF24_250KBPS );
+    Serial.println("2");
     radio.setRetries(3,5); // delay, count
+    Serial.println("3");
     radio.openWritingPipe(slaveAddress);
+    Serial.println("4");
 }
 
 //====================
 
 void loop() {
-    dataToSend = map(analogRead(A0),0, 1023, 0, 255);
+    dataToSend = 0;
     currentMillis = millis();
     if (currentMillis - prevMillis >= txIntervalMillis) {
         send();
@@ -53,7 +58,7 @@ void loop() {
 
 void send() {
     bool rslt;
-    rslt = radio.write( &dataToSend, sizeof(dataToSend) );
+    rslt = radio.write( &dataToSendString, sizeof(dataToSendString) );
         // Always use sizeof() as it gives the size as the number of bytes.
         // For example if dataToSend was an int sizeof() would correctly return 2
 
@@ -65,4 +70,35 @@ void send() {
     else {
         Serial.println("  Tx failed");
     }
+}
+void serialCom(){
+    
+
+    //Check to see if anything is available in the serial receive buffer
+    while (Serial.available() > 0){
+    //Create a place to hold the incoming message
+    static char message[MAX_MESSAGE_LENGTH];
+    static unsigned int message_pos = 0;
+
+    //Read the next available byte in the serial receive buffer
+    char inByte = Serial.read();
+
+    //Message coming in (check not terminating character) and guard for over message size
+    if ( inByte != '\n' && (message_pos < MAX_MESSAGE_LENGTH-1))   {
+        //Add the incoming byte to our message
+        message[message_pos] = inByte;
+        message_pos++;
+        } else {//Full message received...
+            //Add last byte.
+            message[message_pos] = inByte;
+
+            //Print the message (or do other things)
+            Serial.println(message);
+        
+
+            //Reset for the next message
+            message_pos = 0;
+        }
+    }
+
 }
--- src/serialComunication/serialCom.py
+++ src/serialComunication/serialCom.py
@@ -6,13 +6,12 @@ import time
 class SerialCom:
     def __init__(self):
 
-        self.COMPort = "COM10"#deafault com port
-
-
+        self.COMPort = "COM10"#deafault com port. should be set from main.py
         self.arduino = serial.Serial(port=self.COMPort, baudrate=9600, timeout=.1)
         time.sleep(3)#literry dying without this idk.
 
     def cycle(self):
-        self.data = str()#+100 to make sure data uses 3 digits.
-        #self.arduino.write(bytes(self.data, 'utf-8'))
-
+        self.data = str(111222333444)
+        
+        
+        self.arduino.write(bytes(self.data, 'utf-8'))
